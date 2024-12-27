57083212ba980291c6cb87947a2afa0bf5a41811
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-26 21:10:55 -0400

    feat: added some unique indexes

--- model/postgresql/types.go
+++ model/postgresql/types.go
@@ -1,67 +1,67 @@
 package postgresql
 
 import (
-	"database/sql"
 	"gorm.io/gorm"
 	"time"
 )
 
-// User is the PostgreSQL user model
+// User is the PostgreSQL model for the user
 type User struct {
 	gorm.Model
-	UserIDHex string
+	UserIDHex string `json:"user_id_hex" gorm:"uniqueIndex"`
 }
 
-// OrderPayments is the PostgreSQL order payments model
+// OrderPayments is the PostgreSQL model for the order payments
 type OrderPayments struct {
 	gorm.Model
-	OrderID uint64
+	OrderID uint64 `json:"order_id" gorm:"uniqueIndex"`
 }
 
-// OrderPayment is the PostgreSQL order payment model
+// OrderPayment is the PostgreSQL model for the order payment
 type OrderPayment struct {
 	gorm.Model
-	OrderPaymentsID uint64
+	OrderPaymentsID uint64        `json:"order_payments_id"`
 	OrderPayments   OrderPayments `gorm:"foreignKey:OrderPaymentsID"`
-	PaymentID       uint64
-	Payment         Payment `gorm:"foreignKey:PaymentID"`
+	PaymentID       uint64        `json:"payment_id" gorm:"uniqueIndex"`
+	Payment         Payment       `gorm:"foreignKey:PaymentID"`
 }
 
-// BranchRentPayments is the PostgreSQL branch rent payments model
+// BranchRentPayments is the PostgreSQL model for the branch rent payments
 type BranchRentPayments struct {
 	gorm.Model
-	BranchRentIDHex string
+	BranchRentIDHex string `json:"branch_rent_id_hex" gorm:"uniqueIndex"`
 }
 
-// BranchRentPayment is the PostgreSQL branch rent payment model
+// BranchRentPayment is the PostgreSQL model for the branch rent payment
 type BranchRentPayment struct {
 	gorm.Model
-	BranchRentPaymentsID uint64
+	BranchRentPaymentsID uint64             `json:"branch_rent_payments_id"`
 	BranchRentPayments   BranchRentPayments `gorm:"foreignKey:BranchRentPaymentsID"`
-	PaymentID            uint64
-	Payment              Payment `gorm:"foreignKey:PaymentID"`
+	PaymentID            uint64             `json:"payment_id" gorm:"uniqueIndex"`
+	Payment              Payment            `gorm:"foreignKey:PaymentID"`
 }
 
-// Payment is the PostgreSQL payment model
+// Payment is the PostgreSQL model for the payment
 type Payment struct {
 	gorm.Model
-	PaymentAccountID uint64
-	PaymentAccount   PaymentAccount `gorm:"foreignKey:PaymentAccountID"`
-	Amount           float64
-	PaymentDate      time.Time
-	RefundedAt       sql.NullTime
-	VerifiedByUserID sql.NullInt64
-	VerifiedByUser   User `gorm:"foreignKey:VerifiedByUserID"`
+	PaymentAccountID  uint64         `json:"payment_account_id"`
+	PaymentAccount    PaymentAccount `gorm:"foreignKey:PaymentAccountID"`
+	PaymentIdentifier string         `json:"payment_identifier" gorm:"uniqueIndex"`
+	Amount            float64        `json:"amount"`
+	PaymentDate       time.Time      `json:"payment_date"`
+	RefundedAt        *time.Time     `json:"refunded_at,omitempty"`
+	VerifiedByUserID  *uint64        `json:"verified_by_user_id"`
+	VerifiedByUser    User           `gorm:"foreignKey:VerifiedByUserID"`
 }
 
-// PaymentAccount is the PostgreSQL payment account model
+// PaymentAccount is the PostgreSQL model for the payment account
 type PaymentAccount struct {
 	gorm.Model
-	BranchIDHex        sql.NullString
-	AccountPlatform    string
-	AccountNumber      sql.NullString
-	AccountName        sql.NullString
-	AccountEmail       sql.NullString
-	AccountPhoneNumber sql.NullString
-	IsActive           bool
+	BranchIDHex       *string    `json:"branch_id_hex,omitempty"`
+	Platform          string     `json:"platform"`
+	AccountIdentifier string     `json:"account_identifier" gorm:"uniqueIndex"`
+	Owner             *string    `json:"owner,omitempty"`
+	Email             *string    `json:"email,omitempty"`
+	PhoneNumber       *string    `json:"phone_number,omitempty"`
+	SuspendedAt       *time.Time `json:"suspended_at,omitempty"`
 }
